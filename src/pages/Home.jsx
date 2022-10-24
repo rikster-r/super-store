60924df84d62515eca9e7c6ef26c42262be64534
rikster-r <seitdanial43@gmail.com>
2022-10-24 16:50:48 +0600

    Style homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,15 @@
+import "../styles/Home.scss";
+
 export default function Home() {
-  return <></>;
+  return (
+    <main>
+      <div className="info">
+        <div className="slogan">
+          <h1>A Super Store</h1>
+          <h1>for Super People</h1>
+        </div>
+        <button className="btn btn-shop-start">Shop Now</button>
+      </div>
+    </main>
+  );
 }
